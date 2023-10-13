d539a30240756b6a5906d0bc67c70244ba8f0069
Binh314 <binhphamc2002@gmail.com>
2023-10-13 17:19:56 -0400

    fixed registration

--- client/components/Login/RegisterForm.vue
+++ client/components/Login/RegisterForm.vue
@@ -5,13 +5,26 @@ import { ref } from "vue";
 
 const username = ref("");
 const password = ref("");
-const { createUser, loginUser, updateSession } = useUserStore();
+const address = ref("")
+const { createUser, loginUser, updateSession, loginUserCoords } = useUserStore();
 
 async function register() {
-  await createUser(username.value, password.value);
-  await loginUser(username.value, password.value);
-  void updateSession();
-  void router.push({ name: "Home" });
+  if (address.value) {
+    await createUser(username.value, password.value);
+    await loginUser(username.value, password.value, address.value);
+    void updateSession();
+    void router.push({ name: "Home" });
+  } else {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      const location = pos.coords;
+      const latitude = location.latitude;
+      const longitude = location.longitude;
+      await createUser(username.value, password.value);
+      await loginUserCoords(username.value, password.value, latitude, longitude);
+      void updateSession();
+      void router.push({ name: "Home" });
+    });
+  }
 }
 </script>
 
@@ -26,6 +39,10 @@ async function register() {
       <div class="pure-control-group">
         <label for="aligned-password">Password</label>
         <input type="password" v-model.trim="password" id="aligned-password" placeholder="Password" required />
+      </div>
+      <div class="pure-control-group">
+        <label for="aligned-address">Current Location</label>
+        <input type="address" v-model.trim="address" id="aligned-address" placeholder="Address" title="Leave blank to use GPS location."/>
       </div>
       <div class="pure-controls">
         <button type="submit" class="pure-button pure-button-primary">Register</button>
